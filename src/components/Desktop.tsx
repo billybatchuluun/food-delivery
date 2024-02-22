fba99d88e1a972aeacdbd270a5d20dfc355c6145
Bilguun Batchuluun <23LP6327@G304-CC0073.local>
2024-02-22 21:08:10 +0800

    update from class

--- src/components/Desktop.tsx
+++ src/components/Desktop.tsx
@@ -5,22 +5,24 @@ export const Desktop = () => {
     <>
       <div className=" flex bg-[#18BA51] text-white bg-[url('/BgPic.png')]">
         <div className="flex w-1/2 justify-center items-center">
-          <div className="flex  flex-col w-[384px] ">
-            <div>
-              <p className="text-[55px]">Pinecone Food delivery</p>
+          <div className="flex  flex-col w-[384px] mt-[276px] ">
+            <div className="py-5">
+              <p className="text-[55px]">
+                Pinecone <br /> Food delivery
+              </p>
             </div>
             <hr></hr>
-            <div>
+            <div className="py-5">
               <p className="text-[22px]">
                 Horem ipsum dolor sit amet, consectetur adipiscing elit.
               </p>
             </div>
           </div>
         </div>
-        <div className="relative mt-[170px]">
-          <div className="w-[443px] h-[438px]">
+        <div className="w-1/2 mt-[170px]">
+          <div className="w-[443px] h-[438px] relative">
             <img src="./image 13.png" alt="" />
-            <div className="w-[313px] h-[313px] absolute bottom-0 right-0">
+            <div className="w-[313px] h-[313px] absolute bottom-0 -right-40">
               <img src="./image 14.png" alt="" />
             </div>
           </div>
